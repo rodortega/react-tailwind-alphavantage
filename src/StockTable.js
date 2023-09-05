33d16d27501b9f487de130f1cdc9aa4e014ae382
rodortega <rodcarlos2121@gmail.com>
2023-09-05 22:49:56 +0800

    added refresh button

--- src/StockTable.js
+++ src/StockTable.js
@@ -3,9 +3,12 @@ import axios from 'axios';
 
 const StockTable = () => {
   const [stockData, setStockData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); // Add loading state
   const apiKey = 'RIBXT3XYLI69PC0Q';
 
-  useEffect(() => {
+  const fetchData = () => {
+    setIsLoading(true); // Set loading to true while fetching data
+    // Define the stock symbol you want to fetch data for (e.g., AAPL for Apple Inc.).
     const symbol = 'AAPL';
 
     axios
@@ -14,6 +17,7 @@ const StockTable = () => {
       )
       .then((response) => {
         const intradayData = response.data['Time Series (5min)'];
+        // Convert data to an array of objects with all available fields.
         const stockDataArray = Object.entries(intradayData).map(([timestamp, data]) => ({
           timestamp,
           open: parseFloat(data['1. open']),
@@ -23,15 +27,30 @@ const StockTable = () => {
           volume: parseInt(data['5. volume']),
         }));
         setStockData(stockDataArray);
+        setIsLoading(false); // Set loading to false when data is fetched
       })
       .catch((error) => {
         console.error('Error fetching stock data:', error);
+        setIsLoading(false); // Set loading to false in case of an error
       });
+  };
+
+  useEffect(() => {
+    fetchData();
   }, [apiKey]);
 
   return (
     <div className="p-4">
-      <h2 className="text-xl font-semibold mb-4">Stock Intraday Data</h2>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-xl font-semibold">Stock Intraday Data</h2>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          onClick={fetchData}
+          disabled={isLoading} // Disable the button while loading
+        >
+          {isLoading ? 'Refreshing...' : 'Refresh Data'}
+        </button>
+      </div>
       <div className="overflow-x-auto">
         <table className="min-w-full">
           <thead>
@@ -63,7 +82,3 @@ const StockTable = () => {
 };
 
 export default StockTable;
-
-
-
-
